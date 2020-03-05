0c2688e56c8554eda5c6f96641ad34be78c5315e
hklow <skangmy@gmail.com>
2020-03-05 08:42:22 +0800

    Added readme and sample config

---
+++ README.md
@@ -0,0 +1,7 @@
+This tool is to user to get Firebase IdToken for firebase-admin verifyIdToken purposes
+
+Usage:
+
+`node.js <firebase config> <username> <password>`
+
+Please refer to sample-config.js for firebase config
---
+++ sample-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "FIREBASE_API_KEY",
+  authDomain: "PROJECTID.firebaseapp.com",
+  databaseURL: "https://PROJECTID.firebaseio.com",
+  projectId: "PROJECTID",
+  storageBucket: "PROJECTID.appspot.com",
+  appId: "PROJECTID",
+  measurementId: "G-measurement-id"
+};
